ae28fddbc582edbd3e7a98cc801bbee8af4e706e
dell <sara2018170166@cis.asu.edu.eg>
2022-03-01 22:06:32 +0200

    LogIn

--- app/src/main/java/com/example/freshman_guide_chatbot/MainActivity.java
+++ app/src/main/java/com/example/freshman_guide_chatbot/MainActivity.java
@@ -6,10 +6,12 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
